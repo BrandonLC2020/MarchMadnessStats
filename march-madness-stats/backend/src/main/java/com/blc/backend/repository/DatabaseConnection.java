9d1b52b6d25ca5d24c9b7bb1a618dc376ec95a3f
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-03-24 19:27:22 -0400

    Create DatabaseConnection.java

---
+++ march-madness-stats/backend/src/main/java/com/blc/backend/repository/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package com.blc.backend.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import io.github.cdimascio.dotenv.Dotenv;
+    
+public class DatabaseConnection {
+    private static final Dotenv dotenv = Dotenv.load();
+    private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
+
+    private static final String DATABASE_NAME = dotenv.get("DB_NAME");
+    private static final String CLOUD_SQL_PUBLIC_IP = dotenv.get("DB_HOST");
+    private static final String DB_USER = dotenv.get("DB_USER");
+    private static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+
+    private static final String JDBC_URL = "jdbc:mysql://" + CLOUD_SQL_PUBLIC_IP + ":3306/" + DATABASE_NAME;
+
+    public static Connection getConnection() {
+        try {
+            // Load MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            return DriverManager.getConnection(JDBC_URL, DB_USER, DB_PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            // Exception is already logged, no need to print stack trace
+            logger.log(Level.SEVERE, "Database connection error", e);
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+    }
+}
