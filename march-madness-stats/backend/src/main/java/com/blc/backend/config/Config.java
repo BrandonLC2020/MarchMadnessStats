add5136bbe79422db35b4ea1b2744ca1af93054e
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-03-24 12:14:05 -0400

    working with adding the API key

---
+++ march-madness-stats/backend/src/main/java/com/blc/backend/config/Config.java
@@ -0,0 +1,16 @@
+package com.blc.backend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class Config {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    public static String getApiKey() {
+        return dotenv.get("API_KEY");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Your API Key: " + getApiKey()); // For testing purposes only, avoid printing in production.
+    }
+}
+
