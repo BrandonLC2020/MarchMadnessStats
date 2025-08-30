0097faff85be851392ed69a3b17655ffd5c98f2b
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-08-30 09:28:10 -0400

    working on running

--- march-madness-stats/backend/src/main/java/com/blc/backend/BackendApplication.java
+++ march-madness-stats/backend/src/main/java/com/blc/backend/BackendApplication.java
@@ -2,13 +2,15 @@ package com.blc.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-@EnableWebFlux // Enable WebClient for making HTTP requests
 public class BackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("CBB_API_KEY", dotenv.get("CBB_API_KEY"));
         SpringApplication.run(BackendApplication.class, args);
     }
 
