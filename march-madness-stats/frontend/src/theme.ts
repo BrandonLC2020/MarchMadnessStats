667dd0f7cd6fd01504799e61756154e3fc2f3480
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-08-30 18:48:55 -0400

    working on some styling things

---
+++ march-madness-stats/frontend/src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#E67E22', // This is our orange color
+    },
+  },
+});
+
+export default theme;
